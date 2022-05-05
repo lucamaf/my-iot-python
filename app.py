278519a035938981da40a7ea0ccdfb94ecbc4819
lucamaf <lucafe84@hotmail.com>
2022-05-05 23:55:07 +0200

    rollback to last working

--- app.py
+++ app.py
@@ -1,9 +1,5 @@
 #!/usr/bin/env python3
 """a simple sensor data generator that sends to an MQTT broker via paho"""
-# TODO: generate variable payload up to 100kb based on cmd line args
-# reading puback reply, all single threaded to maintain order
-# add support for amqp protocol, payload size
-# implement on_publish to receive the ack
 import json
 import random
 import timeit
@@ -14,18 +10,16 @@ import json
 import time
 import logging
 
+import threading
 from threading import Thread
-from datetime import date, datetime
-
-
 import paho.mqtt.client as mqtt
 
 def on_log(client, userdata, level, buf):
     print("log: ",buf)
 
-# this is the mqtt Producer
-# generating messages
-def generate(host, port, topic, sensors, message, interval,iThread,qos,size):
+# this is the Producer
+# generating messages and reading them at the same time
+def generate(host, port, topic, sensors, message, interval,iThread):
     """generate data and send it to an MQTT broker"""
     # producer client
     mqttc = mqtt.Client(client_id="python-producer")
@@ -43,37 +37,29 @@ def generate(host, port, topic, sensors, message, interval,iThread,qos,size):
 
     keys = list(sensors.keys())
     #print(keys)
-    interval_secs = interval / 1000.0
+    interval_secs = interval/ 1000.0
     loop = 0
     #Start timer
     start = timeit.default_timer()
-    #iterate till the last message
+    #iterate till the end last message
     while loop<(message):
         sensor_id = random.choice(keys)
         
         sensor = sensors[sensor_id]
-        # get a multiple of the sensor payload size based on user input
-        #for s in range(size):
-        #    #print("counter "+str(s));
-        #    s+1;
-
         loop = loop + 1
-        # appending current timestamp to the dict at the beginning of the msg
-        #updict = {"timestamp": datetime.now().isoformat()}
-        #updict.update(sensor)
         payload = json.dumps(sensor)
 
         #Uncomment this to check the sensor signals sent to broker
         # print("PRODUCING: %s: %s" % (topic, payload))
 
-        mqttc.publish(topic, payload, qos=1)
+        mqttc.publish(topic, payload, qos=2)
         time.sleep(interval_secs)
 
     stop = timeit.default_timer()
     #Publish the execution time for pushing the data
     print("Thread" + str(iThread + 1) + "=" + str(round((message / (stop - start)), 2)) + "msg/sec", flush=True)
 
-def main(message,interval,iThread,qos,size):
+def main(message,interval,iThread):
     """main entry point, load and validate config and call generate"""
     config_path = "/cfg/config.json"
     try:
@@ -90,15 +76,15 @@ def main(message,interval,iThread,qos,size):
             port = mqtt_config.get("port", 1883)
             topic = mqtt_config.get("topic", "mqttgen")
 
-            generate(host, port, topic, sensors,message, interval, iThread,qos,size)
+            generate(host, port, topic, sensors,message, interval, iThread)
     except IOError as error:
         print("Error opening config file '%s'" % config_path, error)
 
 if __name__ == '__main__':
-    if len(sys.argv) == 6:
+    if len(sys.argv) == 4:
        #for multithreading
         for iThread in range(int(sys.argv[3])):
-            Thread(target=main, args=(int(sys.argv[1]),int(sys.argv[2]),iThread,int(sys.argv[4]),int(sys.argv[5]))).start();
+            Thread(target=main, args=(int(sys.argv[1]),int(sys.argv[2]),iThread)).start();
         #sys.stdout.flush()
         
     else:
