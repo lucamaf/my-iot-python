128a1e62b037cc8c8ab1852bb4177144130d3d49
lucamaf <lucafe84@hotmail.com>
2022-05-06 08:20:25 +0200

    qos 1 inflight

--- app.py
+++ app.py
@@ -52,7 +52,7 @@ def generate(host, port, topic, sensors, message, interval,iThread):
         #Uncomment this to check the sensor signals sent to broker
         # print("PRODUCING: %s: %s" % (topic, payload))
         mqttc.max_inflight_messages_set(65000)
-        mqttc.publish(topic, payload, 2)
+        mqttc.publish(topic, payload, 1)
         time.sleep(interval_secs)
 
     stop = timeit.default_timer()
